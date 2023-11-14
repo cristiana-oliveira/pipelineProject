80c796a24fbca0b5387c6f18986ffd5714d913c0
cristiana-oliveira <cristiana_aso@hotmail.com>
2023-11-14 16:37:04 +0000

    fixing integration test

--- src/main/java/demo/controller/HelloController.java
+++ src/main/java/demo/controller/HelloController.java
@@ -1,20 +1,26 @@
 package demo.controller;
 
-import org.springframework.util.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.Entity;
+
+import java.util.Collections;
 
 @RestController
 
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String welcome(@RequestParam String name) {
+    @GetMapping(path = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> welcome(@RequestParam String name) {
         if (name == null) {
-           return null;
+            return null;
         } else {
-           return String.format("Hello %s, welcome to my application!", name);
+            return ResponseEntity.status(HttpStatus.CREATED).body(
+                    Collections.singletonMap("name", name));
         }
     }
 }
